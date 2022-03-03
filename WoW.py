143e2d9129cdadb073b14e035014fa9c4f5129f4
Nicholas James <njames22@live.com>
2022-03-03 11:08:50 -0600

    Added SetRealm and Set command

--- WoW.py
+++ WoW.py
@@ -1,24 +1,65 @@
 from asyncio.windows_events import NULL
 import os
+from matplotlib.pyplot import connect
+import requests
+from dotenv import load_dotenv
 #Community API Library for Blizzard
 from blizzardapi import BlizzardApi
-from dotenv import load_dotenv
+
 
 load_dotenv()
 client_id = os.getenv('BLIZZ_ID')
 client_secret = os.getenv('BLIZZ_SECRET')
 api_client = BlizzardApi(client_id, client_secret)
 
+realm_id = 1
+realm_set = False
 
 #Requests realm status data from WoW API
 #Returns a str notifying realm's status
-def ServerStatus():
-    connected_realms = api_client.wow.game_data.get_connected_realm("us", "en_US",11)
-    realms = connected_realms['realms']
-    realms = realms[0]
-    realm_name = realms['name']
-    if (connected_realms['status']['type'] == "UP"):
-        response = f'{realm_name} is up!'
-    elif (connected_realms['status']['type'] != "UP"):
-        response = f'{realm_name} is down!'
-    return response
+
+def SetRealm(user_input):
+    global realm_id, realm_set
+
+    user_input = user_input.replace(' ', '-').lower()
+    realm_list = api_client.wow.game_data.get_realm("us","en_US",user_input)
+
+    if ('Not Found' in realm_list.values()):
+        response = "Realm does not exist, try again."
+        return response
+    else:
+        realm_id = realm_list['id']
+        realm_set = True
+        response = "Realm is set!"
+        print(realm_id)
+        return response
+    
+
+
+
+def RealmStatus():
+    if realm_set == False:
+        response = "Realm not set!"
+        return response
+    
+    connected_realms = api_client.wow.game_data.get_connected_realm("us", "en_US",realm_id)
+    if ('Not Found' in connected_realms.values()):
+        response = "Realm does not exist"
+        print(realm_id)
+        print(connected_realms)
+        return response
+    else:
+        #Collects list of selected realm data
+        realms = connected_realms['realms']
+        #Extracts realm's data from dictionary
+        realms = realms[0]
+        #Extracts name of realm from dictionary
+        realm_name = realms['name']
+
+        #Prints status of realm
+        if (connected_realms['status']['type'] == "UP"):
+            response = f'{realm_name} is up!'
+        elif (connected_realms['status']['type'] != "UP"):
+            response = f'{realm_name} is down!'
+        return response
+
--- bot.py
+++ bot.py
@@ -1,19 +1,25 @@
 # bot.py
+import asyncio
 import os
 import discord
 from discord.ext import commands
 from dotenv import load_dotenv
-from WoW import ServerStatus
+from WoW import SetRealm
+from WoW import RealmStatus
 
+#Pull discord token and approved server from .env
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 
 client = discord.Client()
-bot = commands.Bot(command_prefix='rb!')
+bot = commands.Bot(command_prefix='rb! ')
+
 
 @bot.event
 async def on_ready():
+    SetRealm("NA")
+    RealmStatus()
     for guild in bot.guilds:
         if guild.name == GUILD:
             break
@@ -23,27 +29,30 @@ async def on_ready():
         f'{bot.user.name} is now ready to rock!'
     )
 
-@bot.command(name = 'sleep')
-async def sleep(ctx):
-    await ctx.send('👍')
-    await ctx.send(f'https://tenor.com/view/stanley-sleeping-the-office-gif-10555880')
-    await bot.close()
+
+    
+@bot.command(name = 'set')
+async def set(ctx):
+    await ctx.send(f'Please input your realm')
+
+    def check(msg):
+        return msg.author == ctx.author and msg.channel == ctx.channel
+    
+    msg = await bot.wait_for("message", check=check)
+    realm = msg.content
+    response = SetRealm(realm)
+    await ctx.send(f'{ctx.author.mention}, {response}')
+
+
+        
 
 @bot.command(name = 'status')
 async def status(ctx):
-    response = ServerStatus()
-    if(ctx.author.id == 145984332568199169):
-        await ctx.send(f'Hey Uncle {ctx.author.mention}!, {response}')
-    elif(ctx.author.id == 320025745143365633):
-        await ctx.send(f' {ctx.author.mention} {response}')
-    elif (ctx.author.id == 145989557484126209):
-        await ctx.send(f'In nomine Patris et Filii et Spiritus Sancti. \n Honorable {ctx.author.mention}, {response}')
+    response = RealmStatus()
+    if (ctx.author.id == 145989557484126209):
+        await ctx.send(f'Papa {ctx.author.mention}, {response}')
     else:
         await ctx.send(f'{ctx.author.mention}, {response}')
-
-    role = discord.utils.get(ctx.guild.roles, name="Gal1")
-    if(role in ctx.author.roles):
-        await ctx.send(f'Girl Gamer')
 
 
 bot.run(TOKEN)
